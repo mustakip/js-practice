be99e7d9051c435626f38a75ee6e8ec35a28f1a6
MUSTAKIM PATVEKAR <44019216+mustakip@users.noreply.github.com>
2018-10-22 18:28:13 +0530

    lessened the code by simplifing the pattern handlened almost all errors

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -9,13 +9,13 @@ console.log("-----------------------Welcome to the Game-------------------------
 input();
 let player1 = input("Enter Player - 1 Name :  ");
 let player2 = input("Enter Player - 2 Name :  ");
-console.log(player1+"'s symbol : x ");
-console.log(player2+"'s symbol : o ");
+console.log(player1+"'s symbol : X ");
+console.log(player2+"'s symbol : O ");
 
 const chooseSymbol = function(chance) {
-  symbol = "x";
+  symbol = "X";
   if(chance % 2 != 0) {
-    symbol = "o";
+    symbol = "O";
   }
   return symbol;
 }
@@ -31,67 +31,41 @@ const whosTurn = function(chance) {
 const runGame = function() { 
   let array = new Array(10).fill(" ");
   let assurance = [];
+  let numbers = [1,2,3,4,5,6,7,8,9];
   for(let chance = 0; chance < 9;chance++) {
-    let index = input("Enter a number "+whosTurn(chance)+"   :  ");
-    if(!assurance.includes(index)) {
+    let index = +input("Enter a number "+whosTurn(chance)+"   :  ");
+    if(!assurance.includes(index) && numbers.includes(index)) {
       array[index] = chooseSymbol(chance);
-      console.log(assembleSetup(array));
+      console.clear();
+      console.log(builtGame(array));
       assurance.push(index);
-      if(checkWin(array,array[index]) == true ){
+      if(hasWon(array,array[index])){
         console.log(whosTurn(chance)+" wins the Game..COUGRATULATIONS....!");
         return "Game Over";
       }
+    }else {
+      chance--;
+      console.log("the Block is already takenor invalid number entered try again..")
+
     }
   }
   return "game draw";
 }
 
-const checkWin = function(array,symbol) {
+const hasWon = function(array,symbol) {
   let winningArray = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
   let winStatus = winningArray.some(function(elementArray){let msg = elementArray.every(function(element){return array[element] == symbol;});return msg;})
-  return winStatus;
+  return winStatus == true;
 }
 
-const generateLine = function() {
-  let line = "";
-  for(let no = 0; no < 7; no++){
-    line = line + "_";
-  }
-  return line;
+const builtGame = function(array) {
+  let patternArray = [];
+  patternArray[0] = array[1]+"|"+array[2]+"|"+array[3];
+  patternArray[1] = "-+-+-";
+  patternArray[2] = array[4]+"|"+array[5]+"|"+array[6];
+  patternArray[3] = "-+-+-";
+  patternArray[4] = array[7]+"|"+array[8]+"|"+array[9];
+  return patternArray.join("\n");
 }
-
-const buildSetup = function(array) {
-  let setup = [];
-  let line1 = "";
-  let line2 = "";
-  let line3 = "";
-  for(let no = 1; no < 4; no++) {
-    line1 = line1+"|"+array[no];
-  }
-  for(let no = 4; no < 7; no++) {
-    line2 = line2+"|"+array[no];
-  }
-  for(let no = 7; no < 10; no++) {
-    line3 = line3+"|"+array[no];
-  }
-  line1 = line1 + "|";
-  line2 = line2 + "|";
-  line3 = line3 + "|";
-  setup = [line1,line2,line3];
-  return setup
-}
-
-const assembleSetup = function(array) {
-  let assembly = [];
-  let index = 0;
-  for(let no = 0; no < 6; no += 2) {
-    assembly[no] = generateLine();
-    assembly[no+1] = buildSetup(array)[index];
-    index++;
-  }
-  assembly.push(generateLine());
-  return assembly.join("\n");
-}
-
 
 runGame();
